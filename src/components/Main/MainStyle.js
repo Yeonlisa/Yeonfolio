61c3b3198154cd241b366a18f235d91ebeeea2bb
yeonlisa <tkzw4869@gmail.com>
2022-07-04 16:11:15 +0900

    mainstyle 적용

---
+++ src/components/Main/MainStyle.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const MainStyle = styled.main`
+    position: relative;
+    margin-left: 16.3rem;
+    min-height: 100vh;
+    
+    @media screen and (max-width:1200px){
+        margin-left: 0;
+    }
+`;
