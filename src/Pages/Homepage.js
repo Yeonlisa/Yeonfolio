81d5ee1094c79ec01bb424d6fa766f4364b45174
yeonlisa <tkzw4869@gmail.com>
2022-07-04 18:31:42 +0900

    Homepage 완성

---
+++ src/Pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from '../Components/Home/Home';
+
+const Homepage = () => {
+    return (
+        <>
+            <Home />
+        </>
+    )
+}
+
+export default Homepage
