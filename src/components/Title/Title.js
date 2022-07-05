12c04bdc0e9314cdacfd133a383be2ca8be2d242
yeonlisa <tkzw4869@gmail.com>
2022-07-05 12:34:54 +0900

    Title component 완성

---
+++ src/components/Title/Title.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TitleStyled = styled.div`
+    position: relative;
+`;
+
+const TitleName = styled.h2`
+    color: #fff;
+    font-size: 3.1rem;
+    font-weight: 600;
+    text-transform: uppercase;
+    position: relative;
+    padding-bottom: .5rem;
+    
+    @media screen and (max-width: 496px){
+        font-size: 2.8rem;
+    }
+        
+    @media screen and (max-width: 370px){
+        font-size: 2rem;
+    }
+`;
+
+const Title = ({title}) => {
+    return (
+        <TitleStyled>
+            <TitleName>{title}</TitleName>
+        </TitleStyled>
+    )
+}
+
+export default Title
