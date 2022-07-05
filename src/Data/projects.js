bc77c18d98d67e725e0139e26d9b3ea2a57c32db
yeonlisa <tkzw4869@gmail.com>
2022-07-05 12:36:35 +0900

    project data 완성

---
+++ src/Data/projects.js
@@ -0,0 +1,41 @@
+import css1 from '../Image/Challengers.png';
+import css2 from '../Image/Lollin.png';
+import css3 from '../Image/CSS_Source.png';
+import css4 from '../Image/Flappy-Bird.png';
+
+const projects = [
+    {
+        id: 1,
+        image: css1,
+        link1: "https://github.com/codestates/challengers-client",
+        link2: "http://challengers-codemon.s3-website.ap-northeast-2.amazonaws.com/main",
+        title: 'Challengers',
+        text: '챌린지 공유 서비스'
+    },
+    {
+        id: 2,
+        image: css2,
+        link1: "https://github.com/codestates/Lollin-client",
+        link2: "https://628b8d795a97cd5a3d54ec90--lollin.netlify.app/",
+        title: 'Lollin',
+        text: '리그오브레전드 초보를 위한 길잡이 서비스'
+    },
+    {
+        id: 3,
+        image: css3,
+        link1: "https://github.com/Frontier-Daily/CSS_source",
+        link2: "https://www.youtube.com/c/OnlineTutorials4Designers",
+        title: 'CSS_Source',
+        text: '인터랙티브 효과 클론코딩 모음'
+    },
+    {
+        id: 4,
+        image: css4,
+        link1: "https://github.com/Yeonlisa/Flappy-Bird",
+        link2: "https://yeonlisa.github.io/Flappy-Bird/",
+        title: 'Flappy-Bird',
+        text: '장애물 피하기 게임 서비스'
+    }
+]
+
+export default projects;
