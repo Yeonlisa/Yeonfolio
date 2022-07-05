0388c88989675e5e370ab44c1198c92e09da053a
yeonlisa <tkzw4869@gmail.com>
2022-07-05 13:34:15 +0900

    랜더링 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './Pages/Homepage';
+import Aboutpage from './Pages/Aboutpage';
+import Projectpage from './Pages/Projectpage';
+import Contactpage from './Pages/Contactpage';
+import { MainStyle } from './Components/Main/MainStyle';
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Navbar />
-    </Router>
+      <MainStyle>
+        <Routes>
+          <Route exact path='/' element={<Homepage />} />
+          <Route exact path='/about' element={<Aboutpage />} />
+          <Route exact path='/project' element={<Projectpage />} />
+          <Route exact path='/contact' element={<Contactpage />} />
+        </Routes>
+      </MainStyle>
+    </>
   )
 }
 
