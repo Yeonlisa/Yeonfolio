c0560a550a5afb56bbe1c022e2314a44ac42688a
yeonlisa <tkzw4869@gmail.com>
2022-07-05 18:26:56 +0900

    css 수정

--- src/components/Main/MainStyle.js
+++ src/components/Main/MainStyle.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const MainStyle = styled.main`
     position: relative;
-    margin-left: 16.3rem;
+    margin-left: 1rem;
     min-height: 100vh;
     
     @media screen and (max-width:1200px){
