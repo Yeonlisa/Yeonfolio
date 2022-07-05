f2b9e76de1ad8db70eae0e4e8da2103cde377a6e
yeonlisa <tkzw4869@gmail.com>
2022-07-05 12:32:57 +0900

    layout 완성

---
+++ src/components/Layout/InnerLayout.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const InnerLayout = styled.div`
+    padding: 40px 0;
+`;
---
+++ src/components/Layout/MainLayout.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MainLayout = styled.div`
+    padding: 5rem;
+
+    @media screen and (max-width: 700px){
+        padding: 4rem;
+    }
+
+    @media screen and (max-width: 500px){
+        padding: 2rem .4rem;
+    }
+`;
